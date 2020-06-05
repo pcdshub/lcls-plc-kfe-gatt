c000e06b310678128a844440b5a7ceafff7f9d14
ghalym <ghalym@users.noreply.github.com>
2020-06-05 10:17:29 -0700

    first commit

---
+++ deploy_config.py
@@ -0,0 +1,2 @@
+projects = ['plc-kfe-gatt/plc-kfe-gatt.tsproj']
+local_net_id = "172.17.254.225.1.1"
---
+++ iocBoot/ioc-plc-kfe-gatt/load_plc_databases.cmd
@@ -0,0 +1,1 @@
+dbLoadRecords("plc_kfe_gatt.db", "PORT=ASYN_PLC,PREFIX=PREFIX:,IOCNAME=$(IOCNAME)")
